9084a2049e081cf991f16e63166878cefee5115f
JLloveras <juanlloveras4@gmail.com>
2025-06-25 15:34:28 -0500

    Visuals update

--- bootup.js
+++ bootup.js
@@ -1,10 +1,12 @@
 let image = document.querySelector("img");
+let bg = document.querySelector('body');
 setTimeout(function(){
     image.style.filter="opacity(1)";
+    bg.style.backgroundColor='#001f08';
 }, 1000);
 setTimeout(function(){
     image.style.filter="opacity(0)";
-}, 4000);
+}, 3000);
 setTimeout(function(){
     window.location.replace("http://127.0.0.1:5500/Portfolio/home.html");
-}, 7000);
+}, 5500);
--- script.js
+++ script.js
@@ -2,14 +2,42 @@ var carousel = document.querySelector('.carousel');
 var cellCount = 5;
 var selectedIndex = 0;
 
+var fade1 = document.querySelector("#fade-in");
+var down = document.querySelector('.down');
+setTimeout(function(){
+  fade1.style.filter="opacity(1)";
+  down.style.transform="translateY(0)";
+}, 500)
+
+var fade2 = document.querySelector('#fade-in2');
+setTimeout(function(){
+  fade2.style.transform="translateY(0px)";
+}, 750);
+
+var fade3 = document.querySelector('#fade-in3');
+setTimeout(function(){
+  fade3.style.transform='rotateY(0deg)';
+}, 1750);
+
+var fade4 = document.querySelector('#fade_carousel');
+setTimeout(function(){
+  fade4.style.transform='rotateX(0deg)';
+}, 2000);
+
 function rotateCarousel() {
-  var angle = selectedIndex / cellCount * -360;
-  carousel.style.transform = 'translateZ(-288px) rotateY(' + angle + 'deg)';
+  var angle = selectedIndex / cellCount * -2160;
+  carousel.style.transform = 'translateZ(-241px) rotateY(' + angle + 'deg)';
 }
 
 var prevButton = document.querySelector('.previous-button');
 prevButton.addEventListener( 'click', function() {
   selectedIndex--;
+  rotateCarousel();
+});
+
+var surpriseButton = document.querySelector(".surprise-button");
+surpriseButton.addEventListener( 'click', function() {
+  selectedIndex+=Math.floor(Math.random() *4)+6;
   rotateCarousel();
 });
 
